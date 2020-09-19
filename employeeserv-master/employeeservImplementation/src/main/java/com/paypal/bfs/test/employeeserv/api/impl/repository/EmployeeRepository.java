2523ef1656f429d3e733fb506469506b74d5d58c
Chitranjan Patel <dearchitranjan@gmail.com>
2020-09-18 19:13:03 -0500

    removed for new updated file

--- employeeserv-master/employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/api/impl/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.paypal.bfs.test.employeeserv.api.impl.repository;
-
-import com.paypal.bfs.test.employeeserv.api.impl.repository.entities.EmployeeEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<EmployeeEntity, Integer> {
-}
--- employeeserv-master/employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/api/impl/repository/exception/DuplicateEmployeeException.java
+++
@@ -1,8 +0,0 @@
-package com.paypal.bfs.test.employeeserv.api.impl.repository.exception;
-
-public class DuplicateEmployeeException extends Exception {
-
-    public DuplicateEmployeeException(int id) {
-        super("Employee with employee id " + id + " exists");
-    }
-}
